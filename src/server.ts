0be8b487faba3395e0ddefc61e5896e6dfa9d518
Gabriel <knuckles4x@gmail.com>
2020-10-13 10:54:46 -0300

    database config and first migration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
