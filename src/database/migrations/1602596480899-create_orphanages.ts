e4fc3fd1b6b3661e26381f9a172de2ed8d9f13d3
Gabriel <knuckles4x@gmail.com>
2020-10-13 11:52:34 -0300

    Orphanage model and create

--- src/database/migrations/1602596480899-create_orphanages.ts
+++ src/database/migrations/1602596480899-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1602596480899 implements MigrationInterface {
             type: 'text',
           },
           {
+            name: 'opening_hours',
+            type: 'varchar',
+          },
+          {
             name: 'open_on_weekends',
             type: 'boolean',
             default: false,
